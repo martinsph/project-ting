3bc5517e617bd4450984b7bd50809394a3538bdd
martinsph <ph.mmartins@gmail.com>
2022-04-13 12:10:36 -0300

    add req 4 e 5

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,49 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    report = list()
+
+    for index in range(len(instance)):
+        occurrences = list()
+        path_file = instance.search(index)["nome_do_arquivo"]
+        lines = instance.search(index)
+
+        for line, contents in enumerate(lines["linhas_do_arquivo"]):
+            line_number_counter = line + 1
+            if word.lower() in contents.lower():
+                occurrences.append({"linha": line_number_counter})
+
+    if len(occurrences):
+        word_report = {
+                "palavra": word,
+                "arquivo": path_file,
+                "ocorrencias": occurrences,
+            }
+        report.append(word_report)
+
+    return report
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    report = list()
+
+    for index in range(len(instance)):
+        occurrences = list()
+        path_file = instance.search(index)["nome_do_arquivo"]
+        lines = instance.search(index)
+
+        for line, contents in enumerate(lines["linhas_do_arquivo"]):
+            line_number_counter = line + 1
+            if word.lower() in contents.lower():
+                occurrences.append({
+                    "linha": line_number_counter,
+                    "conteudo": contents
+                    })
+
+    if len(occurrences):
+        word_report = {
+                "palavra": word,
+                "arquivo": path_file,
+                "ocorrencias": occurrences,
+            }
+        report.append(word_report)
+
+    return report
