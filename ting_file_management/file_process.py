900136275bb331c331cb6f4c2fabac3808121050
martinsph <ph.mmartins@gmail.com>
2022-04-13 11:28:44 -0300

    add req 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,35 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for index in range(len(instance)):
+        if instance.search(index)["nome_do_arquivo"] == path_file:
+            return None
+
+    file = txt_importer(path_file)
+
+    content = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": file,
+    }
+
+    print(content)
+    instance.enqueue(content)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if len(instance) != 0:
+        path_file = instance.dequeue()
+        print(f"Arquivo {path_file['nome_do_arquivo']} removido com sucesso")
+    else:
+        print("Não há elementos")
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        print(instance.search(position))
+    except IndexError:
+        # error_msg = "Posição inválida"
+        sys.stderr.write("Posição inválida")
