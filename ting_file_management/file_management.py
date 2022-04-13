2a8254d6a080afeea11609a3d7f07171608bdfec
martinsph <ph.mmartins@gmail.com>
2022-04-13 10:57:57 -0300

    add req 1

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
+
 def txt_importer(path_file):
-    
+    if not path_file.lower().endswith(".txt"):
+        error_msg = "Formato inválido\n"
+        return sys.stderr.write(error_msg)
+
+    try:
+        with open(path_file, "r") as file:
+            return file.read().split("\n")
+
+    except FileNotFoundError:
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,25 @@
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.data = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        return self.data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if self.data:
+            return self.data.pop(0)
+        else:
+            return None
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if self.__len__() == 0:
+            raise IndexError
+        elif index < 0:
+            raise IndexError
+        elif index >= self.__len__():
+            raise IndexError
+        else:
+            return self.data[index]
